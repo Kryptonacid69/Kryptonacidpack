29d91925b7a053f7dd21e8f39fa9a68135cda880
Kryptonacid69 <picklachlanpick@gmail.com>
2026-04-05 01:05:37 +1300

    Add files via upload

---
+++ content/Editions.lua
@@ -0,0 +1,19 @@
+--[[
+SMODS.Shader { 
+  key = 'Green', 
+  path = 'Green.fs' 
+}
+
+SMODS.Edition {
+	key = "Green",
+    loc_txt = {
+        name = "Green",
+        label = "Green",
+        text = {
+            "{X:chips,C:white}x1.5{} {C:blue}Chips{}",
+        }
+    },
+	shader = 'Green',
+}
+
+--]]
---
+++ core.lua
@@ -0,0 +1,33 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: KryptonacidPack
+--- MOD_ID: KryptonacidPack
+--- MOD_AUTHOR: KryptonacidPack
+--- MOD_DESCRIPTION: KryptonacidPack
+--- PREFIX: KryptonacidPack
+----------------------------------------------------------
+----------- MOD CODE -------------------------------------
+
+if not KryptonacidPack then
+    KryptonacidPack = {}
+end
+
+SMODS.current_mod.optional_features = {
+	retrigger_joker = true,
+	post_trigger = true,
+}
+
+local global = {}
+
+local mod_path = "" .. SMODS.current_mod.path
+KryptonacidPack.path = mod_path
+KryptonacidPack_config = SMODS.current_mod.config
+
+local files = NFS.getDirectoryItems(mod_path .. "content")
+for _, file in ipairs(files) do
+	print("[KryptonacidPack] Loading lua file " .. file)
+	local f, err = SMODS.load_file("content/" .. file)
+	if err then
+		error(err) 
+	end
+	f()
+end
